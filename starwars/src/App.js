0a22a24d9a4c4817304caf7b9afc8d677763e4dd
Francis2023 <56661250+Francis2023@users.noreply.github.com>
2020-02-10 21:36:23 -0700

    Not accessing the inner div

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import './App.css';
+import styled from 'styled-components'
+
+
+const card = styled.div`
+  width: 100px;
+  Color: black;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  flex-wrap: wrap;
+  width: 200px;
+  height: 500px;
+
+
+`;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+//const [id, setId] = useState(0);
+const [character, setCharacter] = useState([]);
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+// function CharCard(){
+  useEffect(() => {
+       
+  
 
+     axios   
+       .get(`https://swapi.co/api/people/`)
+          
+       .then(response => {
+            setCharacter(response.data.results) 
+          })
+           
+          .catch(error => console.log(error));
+          },[]);
+
+  console.log("render character data", character)
+  
+  
+  
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <div className="CharacterCard">
+        {character.map(character => (
+           <card key={character.name}>{character.name}</card> 
+        ))}
+        </div>
     </div>
   );
 }
+
+{/* <div className="CharacterCard">
+        {character.map(character => (
+           <div className="card" key={character.name}>{character.name}</div> 
+        ))}
+        </div> */}
 
 export default App;
